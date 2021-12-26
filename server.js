11ee229a1c419512431ba42d60b4c96805ef4c57
Artem Beresnev <artembert@gmail.com>
2021-12-26 18:23:04 +0300

    Serve static via express

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = 3000;
+const ARTIFACTS_FOLDER = "dist";
+const INDEX_HTML = "index.html";
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, ARTIFACTS_FOLDER)));
+app.use("*", (_, res) => {
+  res.sendFile(path.join(__dirname, ARTIFACTS_FOLDER, INDEX_HTML));
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
