2a9e7ca6b0409ed93f5fd011527c71242215ede7
Artem Beresnev <artembert@gmail.com>
2022-02-13 13:05:53 +0300

    [Refactor] Delete unused 'app-input' Handlebar partial

--- src/view-components/index.ts
+++ src/view-components/index.ts
@@ -1,11 +1,9 @@
 import { registerAccountHeaderComponent } from './account-header';
 import { registerAvatarComponent } from './avatar';
-import { registerInputComponent } from './input';
 import { registerLinkComponent } from './link';
 import { registerSearchBarComponent } from './search-bar';
 
 export function registerViewComponents(): void {
-  registerInputComponent();
   registerLinkComponent();
   registerSearchBarComponent();
   registerAvatarComponent();
