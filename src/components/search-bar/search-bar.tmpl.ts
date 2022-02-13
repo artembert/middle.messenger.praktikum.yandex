a9bbcce9130e34fba5af702e7d32a805ecafa866
Artem Beresnev <artembert@gmail.com>
2022-02-13 13:46:30 +0300

    [Feature] Create SearchBar component

---
+++ src/components/search-bar/search-bar.tmpl.ts
@@ -0,0 +1,11 @@
+export const searchBarTemplate = `
+<div class="search-bar">
+  <input
+    class="search-bar__field input-field"
+    type="search"
+    name="{{name}}"
+    value="{{value}}"
+    placeholder="{{placeholder}}"
+  />
+</div>
+`;
