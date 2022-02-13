d3fc953be180be9900f91749eed39c6000e4306f
Artem Beresnev <artembert@gmail.com>
2022-02-13 15:43:19 +0300

    [Refactor] Delete unused Handlebars partials: 'search-bar', 'link'

--- src/view-components/index.ts
+++ src/view-components/index.ts
@@ -1,11 +1,7 @@
 import { registerAccountHeaderComponent } from './account-header';
 import { registerAvatarComponent } from './avatar';
-import { registerLinkComponent } from './link';
-import { registerSearchBarComponent } from './search-bar';
 
 export function registerViewComponents(): void {
-  registerLinkComponent();
-  registerSearchBarComponent();
   registerAvatarComponent();
   registerAccountHeaderComponent();
 }
--- src/view-components/link/link.tmpl.ts
+++
@@ -1,5 +0,0 @@
-export const linkTemplate = `
-<a href="{{href}}" class="button button_mode_{{mode}}" tabindex="0">
-  {{text}}
-</a>
-`;
