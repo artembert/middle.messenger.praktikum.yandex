22d285bce9912e853cdfa7ebc81a8ba99e0edd22
Artem Beresnev <artembert@gmail.com>
2022-03-16 23:29:18 +0300

    feat(routing): save user to store on authorization check

--- src/business-logic/auth/check-authorization.ts
+++ src/business-logic/auth/check-authorization.ts
@@ -1,5 +1,6 @@
 import { AsyncServiceResponse } from '../types/async-service-response.type';
 import { getUserApi } from '../../api/auth/get-user.api';
+import { saveUserToStore } from './save-user-to-store';
 
 interface AuthorizationSuccess extends AsyncServiceResponse {
   isSuccess: true;
@@ -15,6 +16,9 @@ export async function checkAuthorization(): Promise<
   AuthorizationSuccess | AuthorizationFailed
 > {
   const signInAttempt = await getUserApi();
+  if (signInAttempt.isSuccess) {
+    saveUserToStore(signInAttempt.payload);
+  }
   return {
     isSuccess: signInAttempt.isSuccess,
     payload: undefined,
