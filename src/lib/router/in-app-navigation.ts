d25dc17594ba15d72348924519e14af3acceab8e
Artem Beresnev <artembert@gmail.com>
2022-03-16 13:05:38 +0300

    fix(routing): inAppNavigation(): call event.preventDefault before the routing

--- src/lib/router/in-app-navigation.ts
+++ src/lib/router/in-app-navigation.ts
@@ -1,8 +1,8 @@
 import { Router } from './router';
 
 export function inAppNavigation(e: unknown, pathname: string): void {
-  const router = new Router();
-  router.go(pathname);
   (e as Event).preventDefault();
   (e as Event).stopPropagation();
+  const router = new Router();
+  router.go(pathname);
 }
