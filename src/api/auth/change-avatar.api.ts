7b187ef6f0a20ae1879198987a8c4bb09329abf1
Artem Beresnev <artembert@gmail.com>
2022-03-17 23:07:06 +0300

    refactor(api): reorganize api structure

--- src/api/auth/change-avatar.api.ts
+++ src/api/auth/change-avatar.api.ts
@@ -1,9 +1,9 @@
 import { UsersEndpoint } from '../../configs/endpoints.constant';
 import { HttpError } from '../../services/http/http-error';
 import { Http } from '../../services/http/http';
-import { IUserDTO } from '../auth/dto/user.dto';
+import { IUserDTO } from './dto/user.dto';
 import { IUser } from '../../lib/interfaces/user.interface';
-import { userMapper } from '../auth/mappers/user.mapper';
+import { userMapper } from './mappers/user.mapper';
 
 interface IChangeAvatarApiResponseSuccess extends ApiResponse<IUser> {
   isSuccess: true;
